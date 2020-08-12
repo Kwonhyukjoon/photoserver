23b8d09e0d785f5788e0cb46c5d0b14ba59f0ea9
Kwonhyukjoon <victorehko12@naver.com>
2020-08-12 15:19:54 +0900

    commit

--- routes/follow.js
+++ routes/follow.js
@@ -3,6 +3,6 @@ const auth = require("../middleware/auth");
 const { follow } = require("../controllers/follow");
 
 const router = express.Router();
-
+//
 router.route("/").put(auth, follow);
 module.exports = router;
